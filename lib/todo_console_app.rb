570676b55da381104d0a9b7b908a01b502f9f121
Bartek Michalak <barmic12@gmail.com>
2019-03-10 01:42:00 +0100

    Implement part of functionalities.

--- lib/todo_console_app.rb
+++ lib/todo_console_app.rb
@@ -1,6 +1,26 @@
-require "todo_console_app/version"
+#require "todo_console_app/version"
 
 module TodoConsoleApp
   class Error < StandardError; end
-  # Your code goes here...
+
+  class TodoList
+    attr_reader :list
+
+    def initialize
+      @list = Array.new
+    end
+
+    def add_to_list(task)
+      @list.push(task)
+    end
+  end
+
+  class Task
+    attr_reader :title, :completed
+
+    def initialize(title)
+      @title = title
+      @completed = false
+    end
+  end
 end
